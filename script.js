8120522e4ff4b1a7774fa091af5720ccb8ebd086
Hetoreno <hectortamriel@gmail.com>
2020-06-11 22:34:52 -0700

    adding folder

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+for (var e = 0; e < 5; e++) {
+    alert(e);
+}
+*/
+/*
+for (var i = 0; i <= 5; i++){
+    document.write(i + "</br>");
+}
+*/
+
+var tweets = ["hello ", ' yerr' , ' im hungry ', ' ghwatt'];
+
+for (var i = 0; i < tweets.length; i++){
+    document.write(tweets[i] + '</br>');
+}
+
+
